b6567f97a77b83f2a58cc62ddd80f4a0b9091c8d
nabilhzq13 <nabilhzq13@gmail.com>
2026-01-15 22:00:04 +0800

    Create FDC.cpp

---
+++ FDC.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int mainChoice;
+
+    do {
+        cout << "\n==============================================" << endl;
+        cout << "       KINDLE E-READER SYSTEM [VER 0.1]       " << endl;
+        cout << "==============================================" << endl;
+        cout << "1. Buy a Device" << endl;
+        cout << "2. Get E-Book Recommendation" << endl;
+        cout << "3. Check Delivery" << endl;
+        cout << "4. Exit" << endl;
+        cout << "----------------------------------------------" << endl;
+        cout << "Enter choice: ";
+        cin >> mainChoice;
